7ac1a5465688b660fd93941da01d96891de870d9
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-04-27 21:28:57 +0300

    feat: add remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let leng = l;
+  if (l.value === k) {
+    l = l.next
+  }
+
+  while (leng.next) {
+    if (leng.next.value === k) {
+      leng.next = leng.next.next
+    } else {
+      leng = leng.next
+    }
+
+  }
+  return l
 }
 
 module.exports = {
